974058991b570e72d4851fe43993f302941eec7b
Kyon Lee <kyon.h.lee@gmail.com>
2019-09-03 16:29:22 +0900

    Reimplement fetch, save, and set methods in User

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Alan', age: 25 });
+const user = new User({ id: 1, name: 'Alan', age: 23 });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
-
-user.on('change', () => {
-	console.log('Alan has been changed');
+user.on('save', () => {
+	console.log(user);
 });
 
-user.trigger('change');
+user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,5 @@
+import { AxiosResponse } from 'axios';
+
 import { Attributes } from './Attributes';
 import { EventHandler } from './EventHandler';
 import { Sync } from './Sync';
@@ -29,5 +31,31 @@ export class User {
 
 	get get() {
 		return this.attributes.get;
+	}
+
+	set(update: UserProps): void {
+		this.attributes.set(update);
+		this.events.trigger('change');
+	}
+
+	fetch(): void {
+		const id = this.attributes.get('id');
+
+		if (typeof id !== 'number') {
+			throw new Error('Cannot fetch without an id');
+		}
+
+		this.sync.fetch(id).then((response: AxiosResponse): void => {
+			this.set(response.data);
+		});
+	}
+
+	save(): void {
+		this.sync
+			.save(this.attributes.getAll())
+			.then((response: AxiosResponse) => {
+				this.events.trigger('save');
+			})
+			.catch(() => this.events.trigger('error'));
 	}
 }
