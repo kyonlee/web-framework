47f5385e0b26e2f121b04a31fe0c75021f3bd6e2
Kyon Lee <kyon.h.lee@gmail.com>
2019-09-05 18:19:11 +0900

    Add UserForm view class

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,4 @@
-// import { User } from './models/User';
+import { UserForm } from './views/UserForm';
 
-// const user = User.buildUser({ id: 2 });
-
-// user.on('change', () => {
-// 	console.log(user);
-// });
-
-// user.fetch();
-
-import { User } from './models/User';
-
-const collection = User.buildUserCollection();
-
-collection.on('change', () => {
-	console.log(collection);
-});
-
-collection.fetch();
-collection.fetch();
+const userForm = new UserForm(document.getElementById('root'));
+userForm.render();
---
+++ src/views/UserForm.ts
@@ -0,0 +1,20 @@
+export class UserForm {
+	constructor(public parent: Element) {}
+
+	template(): string {
+		return `
+            <div>
+                <h1>User form</h1>
+                <input />
+                <button>Click Me!</button>
+            </div>
+        `;
+	}
+
+	render(): void {
+		const templateElement = document.createElement('template');
+		templateElement.innerHTML = this.template();
+
+		this.parent.append(templateElement.content);
+	}
+}
