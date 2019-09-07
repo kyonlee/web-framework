9360b1d1a5fb66215ec04cb2a85ca8c0b844d8fc
Kyon Lee <kyon.h.lee@gmail.com>
2019-09-07 17:28:59 +0900

    Bind event handlers

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,6 +1,16 @@
 export class UserForm {
 	constructor(public parent: Element) {}
 
+	eventsMap(): { [key: string]: () => void } {
+		return {
+			'click:button': this.onButtonClick
+		};
+	}
+
+	onButtonClick(): void {
+		console.log('I have been clicked');
+	}
+
 	template(): string {
 		return `
             <div>
@@ -11,10 +21,23 @@ export class UserForm {
         `;
 	}
 
+	bindEvents(fragment: DocumentFragment): void {
+		const eventsMap = this.eventsMap();
+
+		for (let eventKey in eventsMap) {
+			const [eventName, selector] = eventKey.split(':');
+
+			fragment.querySelectorAll(selector).forEach(element => {
+				element.addEventListener(eventName, eventsMap[eventKey]);
+			});
+		}
+	}
+
 	render(): void {
 		const templateElement = document.createElement('template');
 		templateElement.innerHTML = this.template();
 
+		this.bindEvents(templateElement.content);
 		this.parent.append(templateElement.content);
 	}
 }
